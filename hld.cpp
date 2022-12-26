7cbfb5d9e75a59a53eca4e5b1c95f39dd9198a19
Kunal Shrivastva <88685195+kunalsrv20@users.noreply.github.com>
2022-12-27 01:28:44 +0530

    Create hld.cpp

---
+++ hld.cpp
@@ -0,0 +1,6 @@
+// Queries on Tree are of different types
+// A few of the types are Queries on subtrees and Queries on Path between two nodes
+// HLD is primarily used to answer quries related to path between two nodes.
+
+// How to linearize the tree in the form of the array ?
+// A path between two nodes can be represented by at most logN different concatenation of [L, R]
